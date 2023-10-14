4162cce02da61ff7955f3dbf037c9bb5ec207021
Argenis De La Rosa <oftherose91@icloud.com>
2023-10-14 13:05:57 -0400

    fixed the folder structure

---
+++ scripts/cocktail-db.js
@@ -0,0 +1,14 @@
+class CocktailAPI {
+
+    constructor(apiKey) {
+
+        this.baseURL = "https://www.thecocktaildb.com";
+        this.apiKey = apiKey
+    }
+
+    getCocktails() {
+        
+    }
+
+    
+}
